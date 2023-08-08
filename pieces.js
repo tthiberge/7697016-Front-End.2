36fa97a79ebf5202a2b74586a7a2d4e4bbb8b776
Theophile Thiberge <tthiberge@gmail.com>
2023-08-08 16:30:23 +0200

    Fonction map et splice

--- pieces.js
+++ pieces.js
@@ -25,7 +25,7 @@ for (let i = 0; i < pieces.length; i++) {
 
     // On rattache la balise article a la section Fiches
     sectionFiches.appendChild(pieceElement);
-    // On rattache l’image à pieceElement (la balise article et non plus la section fiches)
+    // On rattache l’image à pieceElement (la balise article)
     pieceElement.appendChild(imageElement);
     pieceElement.appendChild(nomElement);
     pieceElement.appendChild(prixElement);
@@ -33,42 +33,83 @@ for (let i = 0; i < pieces.length; i++) {
     //Ajout des éléments au DOM pour l'exercice
     pieceElement.appendChild(descriptionElement);
     pieceElement.appendChild(stockElement);
-}
 
+ }
+
+ //gestion des boutons
 const boutonTrier = document.querySelector(".btn-trier");
+
 boutonTrier.addEventListener("click", function () {
-  const piecesOrdonnees = Array.from(pieces);
-  piecesOrdonnees.sort(function (a, b) {
-      return a.prix - b.prix;
-   });
-   console.log(piecesOrdonnees);
+    const piecesOrdonnees = Array.from(pieces);
+    piecesOrdonnees.sort(function (a, b) {
+        return a.prix - b.prix;
+     });
+     console.log(piecesOrdonnees);
 });
 
 const boutonFiltrer = document.querySelector(".btn-filtrer");
 
 boutonFiltrer.addEventListener("click", function () {
-   const piecesFiltrees = pieces.filter(function (piece) {
-       return piece.prix <= 35;
-   });
+    const piecesFiltrees = pieces.filter(function (piece) {
+        return piece.prix <= 35;
+    });
+   console.log(piecesFiltrees)
+});
 
+const boutonDecroissant = document.querySelector(".btn-decroissant");
+boutonDecroissant.addEventListener("click", function () {
+    const piecesOrdonnees = Array.from(pieces);
+    piecesOrdonnees.sort(function (a, b) {
+        return b.prix - a.prix;
+     });
+     console.log(piecesOrdonnees);
+});
+
+const boutonNoDescription = document.querySelector(".btn-nodesc");
+boutonNoDescription.addEventListener("click", function () {
+    const piecesFiltrees = pieces.filter(function (piece) {
+        return piece.description;
+    });
    console.log(piecesFiltrees);
-})
+});
+
+const noms = pieces.map(piece => piece.nom);
+for(let i = pieces.length -1 ; i >= 0; i--){
+    if(pieces[i].prix > 35){
+        noms.splice(i,1);
+    }
+}
+
+//Création de la liste
+const abordablesElements = document.createElement('ul');
+//Ajout de chaque nom à la liste
+for(let i=0; i < noms.length ; i++){
+    const nomElement = document.createElement('li');
+    nomElement.innerText = noms[i];
+    abordablesElements.appendChild(nomElement)
+}
+// Ajout de l'en-tête puis de la liste au bloc résultats filtres
+document.querySelector('.abordables')
+    .appendChild(abordablesElements)
 
 
-// Fonctionne
-const btnDecroissant = document.querySelector(".btn-trier-decroissant");
-btnDecroissant.addEventListener("click", function() {
-  const piecesDecroissant = Array.from(pieces);
-  piecesDecroissant.sort(function(a, b) {
-    return b.prix - a.prix;
-  });
-  console.log(piecesDecroissant);
-})
+//Code Exercice
+const nomsDisponibles = pieces.map(piece => piece.nom);
+const prixDisponibles = pieces.map(piece => piece.prix);
 
-const BtnDescription = document.querySelector(".btn-filtrer-description")
-BtnDescription.addEventListener("click", function () {
-  const piecesFiltreesDescription = pieces.filter(function (piece) {
-    return piece.description // Suffit pour demander si c'est présent, si ça existe
-  })
-  console.log(piecesFiltreesDescription);
-})
+for(let i = pieces.length -1 ; i >= 0; i--){
+    if(pieces[i].disponibilite === false){
+        nomsDisponibles.splice(i,1);
+        prixDisponibles.splice(i,1);
+    }
+}
+
+const disponiblesElement = document.createElement('ul');
+
+for(let i=0 ; i < nomsDisponibles.length ; i++){
+    const nomElement = document.createElement('li');
+    nomElement.innerText = `${nomsDisponibles[i]} - ${prixDisponibles[i]} €`;
+    disponiblesElement.appendChild(nomElement);
+}
+
+document.querySelector('.disponibles').appendChild(disponiblesElement);
